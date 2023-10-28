44b3289ff7bbbb95fea374ea9d2d1a021a08fc18
Manolo <manoloramos@pm.me>
2023-10-28 15:51:05 +0200

    Added Home page to App

--- src/App.tsx
+++ src/App.tsx
@@ -4,32 +4,17 @@
  *  Copyright: (C) 2023 Manolo Ramos - All Rights Reserved
  * 
  *  File created on: 24-10-2023 12:49:20
- *  Last modified on: 28-10-2023 15:14:24
+ *  Last modified on: 28-10-2023 15:36:40
  * 
- *  Description: App File
+ *  Description: Main App File
  */
 
-import reactLogo from '@assets/icons/react.svg';
-import viteLogo from '@assets/icons/vite.svg';
 import '@styles/App.css';
-import Container from '@components/layout';
+import Home from '@/pages';
 
 const App: React.FC = () => {
-
 	return (
-		<Container>
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-
-				<h2>Hello World!</h2>
-				<h3>This site is beign developed using React + Vite</h3>
-			</div>
-		</Container>
+		<Home />
 	)
 };
 
