2798778e9c913c47600bbb9fc75d50d7ccf27f39
Manolo <manoloramos@pm.me>
2023-10-28 15:49:13 +0200

    Fixed import

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,14 @@
  *  Copyright: (C) 2023 Manolo Ramos - All Rights Reserved
  * 
  *  File created on: 24-10-2023 12:49:20
- *  Last modified on: 28-10-2023 15:15:11
+ *  Last modified on: 28-10-2023 15:37:04
  * 
  *  Description: Renders the Application in the DOM.
  */
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import App from '@/App.tsx';
 import '@styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
