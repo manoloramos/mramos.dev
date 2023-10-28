087c46cd4ebd3f13fe0f967a64884e56d2177c09
Manolo <manoloramos@pm.me>
2023-10-28 15:21:11 +0200

    Added layout components index

---
+++ src/components/layout/index.ts
@@ -0,0 +1,14 @@
+/**
+ *  Project: manoloramos.dev
+ *  Author: Manolo Ramos
+ *  Copyright: (C) 2023 Manolo Ramos - All Rights Reserved
+ * 
+ *  File created on: 24-10-2023 20:20:05
+ * @ Last modified on: 25-10-2023 19:48:09
+ * 
+ *  Description: Main Layout component file
+ */
+
+import Container from './Container/Container';
+
+export default Container;
