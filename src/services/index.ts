6377f6887e9b58322671bd5d87f37ac32ba2a6d4
Manolo <manoloramos@pm.me>
2023-10-28 15:50:43 +0200

    Index files added

---
+++ src/services/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Project: manoloramos.dev
+ * Author: Manolo Ramos
+ * Copyright: (C) 2023 Manolo Ramos - All Rights Reserved
+ * 
+ * File created on: 25-10-2023 19:57:38
+ * Last modified on: 25-10-2023 19:57:40
+ * 
+ * Description: Index file for Services
+ */
+
