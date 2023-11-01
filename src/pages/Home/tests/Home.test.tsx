9708e9ddfbcecbf0940cb2ecaf8810adb06ce1fd
Manolo <manoloramos@pm.me>
2023-11-01 13:52:41 +0100

    Added Home page test file

--- src/pages/Home/tests/Home.test.tsx
+++ src/pages/Home/tests/Home.test.tsx
@@ -4,20 +4,23 @@
  *  Copyright: (C) 2023 Manolo Ramos - All Rights Reserved
  * 
  *  File created on: 25-10-2023 20:05:07
- *  Last modified on: 31-10-2023 13:40:00
+ *  Last modified on: 01-11-2023 13:47:01
  * 
- *  Description: Home page tests
+ *  Description: Test file for the Home page.
  */
 
-import { render } from "@testing-library/react";
+import '@testing-library/jest-dom';
+import { render, waitFor } from "@testing-library/react";
 import Home from "../Home";
 
 describe('Home page', () => {
 	it('should render the page',() => {
-		const homePage = render(
+		const home = render(
 			<Home />
 		);
 
-		expect(homePage).toBeInTheDocument();
+		waitFor(() => {
+			expect(home).toBeInTheDocument();
+		});
 	});
 });
