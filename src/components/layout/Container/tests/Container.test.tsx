ea3b0906de52dea2f5f1256fd8864923d644f72a
Manolo <manoloramos@pm.me>
2023-11-01 13:52:51 +0100

    Added Container component test file

--- src/components/layout/Container/tests/Container.test.tsx
+++ src/components/layout/Container/tests/Container.test.tsx
@@ -4,8 +4,24 @@
  *  Copyright: (C) 2023 Manolo Ramos - All Rights Reserved
  * 
  *  File created on: 24-10-2023 20:57:11
- *  Last modified on: 24-10-2023 20:57:21
+ *  Last modified on: 01-11-2023 13:44:52
  * 
- *  Description:
+ *  Description: Test file for the Container component.
  */
 
+import { render, waitFor } from "@testing-library/react";
+import Container from "../Container";
+
+describe('Container component', () => {
+	it('should render the component',() => {
+		const container = render(
+			<Container>
+				<span>Test</span>
+			</Container>
+		);
+
+		waitFor(() => {
+			expect(container).toBeInTheDocument();
+		});
+	});
+});
