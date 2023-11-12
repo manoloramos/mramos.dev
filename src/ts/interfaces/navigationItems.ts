f40281e2e70352471a8eaf6ee285346b43567a60
Manolo <manoloramos@pm.me>
2023-11-12 12:03:59 +0100

    ✨ feature: Add navigationItems interface

---
+++ src/ts/interfaces/navigationItems.ts
@@ -0,0 +1,17 @@
+/**
+ *  Project: manoloramos.dev
+ *  Author: Manolo Ramos
+ *  Copyright: (C) 2023 Manolo Ramos - All Rights Reserved
+ * 
+ *  File created on: 11-11-2023 12:46:53
+ * @ Last modified on: 12-11-2023 11:48:36
+ * 
+ *  Description: Structure used to define common navigation items.
+ */
+
+interface navigationItems {
+	url: string,
+	label: string
+}
+
+export { type navigationItems };
