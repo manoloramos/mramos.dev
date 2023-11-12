00548b2ed206a17230d2d9772249de535c6fdb23
Manolo <manoloramos@pm.me>
2023-11-12 21:25:10 +0100

    ✨ feature: Added index file

---
+++ src/components/sections/index.ts
@@ -0,0 +1,15 @@
+/**
+ *  Project: manoloramos.dev
+ *  Author: Manolo Ramos
+ *  Copyright: (C) 2023 Manolo Ramos - All Rights Reserved
+ * 
+ *  File created on: 12-11-2023 19:43:46
+ * @ Last modified on: 12-11-2023 19:49:39
+ * 
+ *  Description: Index file for section components.
+ */
+
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+
+export { Footer, Header };
