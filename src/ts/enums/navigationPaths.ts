57d1b9324f718b5833b3c4e53a84911fc8a6a43d
Manolo <manoloramos@pm.me>
2023-11-12 12:04:15 +0100

    ✨ feature: Add navigationPaths enum

---
+++ src/ts/enums/navigationPaths.ts
@@ -0,0 +1,20 @@
+/**
+ *  Project: manoloramos.dev
+ *  Author: Manolo Ramos
+ *  Copyright: (C) 2023 Manolo Ramos - All Rights Reserved
+ * 
+ *  File created on: 11-11-2023 12:43:39
+ * @ Last modified on: 11-11-2023 20:26:59
+ * 
+ *  Description:  Defines the elements of the navigation header.
+ */
+
+enum NavPaths {
+	About = '/about',
+	Blog = '/blog',
+	Portfolio = '/portfolio',
+	GitHub = '#',
+	Contact = '/contact'
+}
+
+export { NavPaths };
