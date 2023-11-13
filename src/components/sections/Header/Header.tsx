a061abcf2b840f95dea80b2eb4b351372f80d83b
Manolo Ramos <35995905+manoloramos@users.noreply.github.com>
2023-11-13 18:13:18 +0100

    ✨ feature: Added header styles & navigation items rendering

--- src/components/sections/Header/Header.tsx
+++ src/components/sections/Header/Header.tsx
@@ -4,30 +4,80 @@
  *  Copyright: (C) 2023 Manolo Ramos - All Rights Reserved
  * 
  *  File created on: 06-11-2023 12:49:52
- *  Last modified on: 09-11-2023 23:33:46
+ * @ Last modified on: 13-11-2023 17:35:13
  * 
  *  Description: Header component. Contains the main navigation elements and the light/dark toggle.
  */
 
-import React from "react";
+import { NavPaths } from '@/ts/enums/navigationPaths';
+import React from 'react';
+import _ from 'lodash';
+import externalLink from '@assets/icons/external_link.svg';
+import logo from '@assets/images/logo/logo_sm.png';
+import { navigationItems } from '@ts/interfaces/navigationItems';
+import styles from './Header.styles';
 
 interface HeaderProps {
-	navElements: string
+	themeSwitch: () => void;
 }
 
-const Header: React.FC<HeaderProps> = () => {
-	return(
-		<header className="navbar">
-			<div className="navbar-container">
-				<div>
-					<a href="#">
-						<img src="" alt="" />
+const Header: React.FC<HeaderProps> = ({
+	themeSwitch
+}) => {
+	
+	const mainNavItems: navigationItems[] = [
+		{ url: NavPaths.About, label: 'About me' },
+		{ url: NavPaths.Blog ,label: 'Blog' },
+		{ url: NavPaths.Portfolio, label: 'Portfolio' }
+	];
+
+	const externalNavItems: navigationItems[] = [
+		{ url: NavPaths.GitHub, label: 'GitHub', icon: externalLink }
+	];
+	
+	return (
+		<header css={styles.header}>
+			<div css={styles.headerContainer}>
+				<div css={styles.navbarContainer}>
+					<a href={NavPaths.Home}>
+						<img src={logo} alt="Logo"/>
+						<span>Manolo Ramos</span>
 					</a>
-					<nav></nav>
+					<nav>
+						{
+							_.map(mainNavItems, (item, key) => (
+								<div key={key}>
+									<a href={item.url}>
+										<span>{item.label}</span>
+									</a>
+								</div>
+							))
+						}
+						
+						{
+							_.map(externalNavItems, (item, key) => (
+								<div key={key}>
+									<a 
+										href={item.url}
+										target='_blank'
+										rel='noreferrer'
+									>
+										<span>{item.label}</span>
+										{ item.icon &&
+											<img 
+												src={item.icon} 
+												alt="Nav item icon"
+											/>
+										}
+									</a>
+								</div>
+							))
+						}
+					</nav>
 				</div>
-				<div>
-					<button>
-						<img src="" alt="" />
+				<div css={styles.themeButtonContainer}>
+					<button onClick={themeSwitch}>
+						<img src="#" alt="Theme switch" />
 					</button>
 				</div>
 			</div>
