ee6de47c959defbe287d3806330b16f345caf636
Manolo Ramos <35995905+manoloramos@users.noreply.github.com>
2023-11-27 11:00:00 +0100

    ✨ feature: added generic navigation enum

---
+++ src/ts/enums/navigation.ts
@@ -0,0 +1,27 @@
+/**
+ *  Project: manoloramos.dev
+ *  Author: Manolo Ramos
+ *  Copyright: (C) 2023 Manolo Ramos - All Rights Reserved
+ * 
+ *  File created on: 13-11-2023 23:02:38
+ *  Last modified on: 13-11-2023 23:34:39
+ * 
+ *  Description: Enums used in navigation related components.
+ */
+
+enum NavPaths {
+	Home = '/',
+	About = '/about',
+	Blog = '/blog',
+	Portfolio = '/portfolio',
+	GitHub = 'https://github.com/manoloramos',
+	Contact = '/contact'
+}
+
+enum FooterLinks {
+	Vite = 'https://vitejs.dev',
+	Netlify = 'https://app.netlify.com/sites/manoloramos/deploys',
+	GitHub = 'https://github.com/manoloramos'
+}
+
+export { FooterLinks, NavPaths };
