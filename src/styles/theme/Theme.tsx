9e7f97c0345c003c23fab6b97ba76f9f38e87f33
Manolo Ramos <35995905+manoloramos@users.noreply.github.com>
2023-12-07 20:14:57 +0100

    ✨ feature: define theme file

---
+++ src/styles/theme/Theme.tsx
@@ -0,0 +1,20 @@
+/**
+ *  Project: manoloramos.dev
+ *  Author: Manolo Ramos
+ *  Copyright: (C) 2023 Manolo Ramos - All Rights Reserved
+ * 
+ *  File created on: 7-12-2023 20:11:13 
+ *  Last modified on: 7-12-2023 20:14:40 
+ * 
+ *  Description: Defines all the available themes for the App.
+ */
+
+const light = {
+
+};
+
+const dark = {
+
+};
+
+export { light, dark };
