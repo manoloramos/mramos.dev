1ae307e885d2a52a76d63a35220a88d6ae4e9840
Manolo Ramos <35995905+manoloramos@users.noreply.github.com>
2023-12-23 11:33:20 +0100

    🔧 config: sorted vite config imports

--- vite.config.ts
+++ vite.config.ts
@@ -10,8 +10,8 @@
  */
 
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import path from 'path';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
