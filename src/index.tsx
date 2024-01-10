a0723b40cbf8fba6afd7ddfce590d230c7788c9b
Manolo Ramos <35995905+manoloramos@users.noreply.github.com>
2024-01-11 00:21:53 +0100

    🧹 cleanup: removed stray comma

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@
  *  Copyright: (C) 2023 Manolo Ramos - All Rights Reserved
  * 
  *  File created on: 24-10-2023 12:49:20
- *  Last modified on: 8-12-2023 11:45:39 
+ *  Last modified on: 10-1-2024 23:40:04 
  * 
  *  Description: Renders the Application in the DOM.
  */
@@ -17,5 +17,5 @@ import '@styles/index.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<App />
-	</React.StrictMode>,
+	</React.StrictMode>
 );
