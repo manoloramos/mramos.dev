6b6d4ca0c83346cb66771585131e07fd17f8d8e1
Manolo Ramos <35995905+manoloramos@users.noreply.github.com>
2024-01-11 00:15:37 +0100

    🔧 config: added theme path to viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
  * Copyright: (C) 2023 Manolo Ramos - All Rights Reserved
  * 
  * File created on: 24-10-2023 12:49:20
- * Last modified on: 25-10-2023 19:42:05
+ * Last modified on: 11-1-2024 00:15:18 
  * 
  * Description: Vite config file
  */
@@ -15,18 +15,18 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-
-    alias: {
-      '@': path.resolve(__dirname, './src/'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@services': path.resolve(__dirname, './src/services'),
-      '@styles': path.resolve(__dirname, './src/styles'),
-      '@types': path.resolve(__dirname, './src/types'),
-      '@utils': path.resolve(__dirname, './src/utils'),
-    }
-  }
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src/'),
+			'@assets': path.resolve(__dirname, './src/assets'),
+			'@components': path.resolve(__dirname, './src/components'),
+			'@pages': path.resolve(__dirname, './src/pages'),
+			'@services': path.resolve(__dirname, './src/services'),
+			'@styles': path.resolve(__dirname, './src/styles'),
+			'@theme': path.resolve(__dirname, './src/styles/theme/Theme'),
+			'@types': path.resolve(__dirname, './src/types'),
+			'@utils': path.resolve(__dirname, './src/utils'),
+		}
+	}
 });
