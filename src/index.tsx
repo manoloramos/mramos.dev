f7ee05d8f995220f8ce1ad0b1d67c76b601a6255
Manolo Ramos <35995905+manoloramos@users.noreply.github.com>
2024-01-11 18:06:48 +0100

    ⚰️ remove: removed invalid index.css import

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@
  *  Copyright: (C) 2023 Manolo Ramos - All Rights Reserved
  * 
  *  File created on: 24-10-2023 12:49:20
- *  Last modified on: 10-1-2024 23:40:04 
+ *  Last modified on: 11-1-2024 18:02:51 
  * 
  *  Description: Renders the Application in the DOM.
  */
@@ -12,7 +12,6 @@
 import App from '@/App.tsx';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import '@styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
