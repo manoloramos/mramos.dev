cc04f772d7b85744de841e405478eb6dacfcccb1
Manolo Ramos <35995905+manoloramos@users.noreply.github.com>
2024-01-11 18:48:13 +0100

    ✨ feature: added globalStyles file

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,49 @@
+/**
+ *  Project: manoloramos.dev
+ *  Author: Manolo Ramos
+ *  Copyright: (C) 2024 Manolo Ramos - All Rights Reserved
+ * 
+ *  File created on: 11-1-2024 18:41:37 
+ *  Last modified on: 11-1-2024 18:41:52 
+ * 
+ *  Description: Defines the global styles used by the App.
+ */
+
+import { css } from "@emotion/react";
+
+const globalStyles = css`
+	:root {
+		font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+		line-height: 1.5;
+		font-weight: 400;
+	
+		color-scheme: light dark;
+		color: rgba(255, 255, 255, 0.87);
+		background-color: #242424;
+	
+		font-synthesis: none;
+		text-rendering: optimizeLegibility;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		-webkit-text-size-adjust: 100%;
+	}
+	
+	#root {
+		max-width: 1200px;
+		width: 100%;
+		margin: 0 auto;
+		padding: 2rem;
+		text-align: center;
+	}
+
+	body {
+		margin: 0;
+		display: flex;
+		place-items: center;
+		min-width: 320px;
+		min-height: 100vh;
+		overflow-x: hidden;
+	}
+`;
+
+export default globalStyles;
