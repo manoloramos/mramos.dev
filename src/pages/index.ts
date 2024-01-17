55aa1e4a49d82b56ffa13f6037b86d32e9a754b1
Manolo Ramos <35995905+manoloramos@users.noreply.github.com>
2024-01-17 21:56:10 +0100

    ♻️ refactor: updated Home page import on Pages index

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -4,11 +4,11 @@
  *  Copyright: (C) 2023 Manolo Ramos - All Rights Reserved
  * 
  *  File created on: 28-10-2023 15:26:38
- * @ Last modified on: 28-10-2023 15:27:28
+ * @ Last modified on: 17-1-2024 21:49:24 
  * 
  *  Description: Index file for Pages
  */
 
-import Home from '@pages/Home/Home';
+import Home from './Home/Home';
 
 export default Home;
