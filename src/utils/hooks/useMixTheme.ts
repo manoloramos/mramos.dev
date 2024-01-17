f633c15b166a9e8677ec6962d7aef2e5ab107e18
Manolo Ramos <35995905+manoloramos@users.noreply.github.com>
2024-01-17 21:19:31 +0100

    ✨ feature: added useMixTheme hook

--- src/utils/hooks/useMixTheme.ts
+++ src/utils/hooks/useMixTheme.ts
@@ -4,21 +4,18 @@
  *  Copyright: (C) 2024 Manolo Ramos - All Rights Reserved
  * 
  *  File created on: 15-1-2024 19:37:00 
- *  Last modified on: 15-1-2024 20:24:00 
+ *  Last modified on: 17-1-2024 21:18:30 
  * 
- *  Description: This hook provides functionality aimed to simplify the style overriding proccess when any theme properties are used in a component.
+ *  Description: This hook provides functionality aimed to simplify the style overriding proccess whenever any theme properties are used in a component.
  */
 
 import { CSSObject } from '@emotion/react';
-import { ThemeProperties } from '@/ts/interfaces/theme';
-import { useTheme } from '@emotion/react';
+import { StyleRuleset } from '@/ts/interfaces/theming';
 
-const useMixTheme = (componentStyles: object, themeProperty: keyof ThemeProperties): CSSObject => {
-  const theme = useTheme() as ThemeProperties;
-  return {
-    ...componentStyles,
-    background: theme[themeProperty],
-  };
+const useMixTheme = (componentStyles: object, themeProperty: StyleRuleset): CSSObject => {
+	const mixedStyles = { ...componentStyles, ...themeProperty };
+
+	return mixedStyles;
 };
 
 export default useMixTheme;
