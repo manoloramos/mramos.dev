2c529a5fd3865cbe8fe10cdac85088de628f1a24
Manolo Ramos <35995905+manoloramos@users.noreply.github.com>
2024-01-18 23:37:01 +0100

    ✨ feature: added footerItems file

---
+++ src/data/navigation/footerItems.ts
@@ -0,0 +1,24 @@
+/**
+ *  Project: manoloramos.dev
+ *  Author: Manolo Ramos
+ *  Copyright: (C) 2024 Manolo Ramos - All Rights Reserved
+ * 
+ *  File created on: 18-1-2024 23:19:54 
+ *  Last modified on: 18-1-2024 23:32:01 
+ * 
+ *  Description: Footer items.
+ */
+
+import { FooterLinks } from "@/ts/enums/navigation";
+import github from '@assets/icons/github_lm.svg';
+import { navigationItems } from "@ts/interfaces/navigationItems";
+import netlify from '@assets/icons/netlify_lm.svg';
+import vite from '@assets/icons/vite.svg';
+
+const footerNavItems: navigationItems[] = [
+	{ url: FooterLinks.Vite, label: 'Vite', icon: vite},
+	{ url: FooterLinks.Netlify ,label: 'Netlify', icon: netlify },
+	{ url: FooterLinks.GitHub, label: 'GitHub', icon: github },
+];
+
+export default footerNavItems;
