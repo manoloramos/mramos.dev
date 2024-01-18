1b0740967d8255f61a24583dba7aec57a57f6ea9
Manolo Ramos <35995905+manoloramos@users.noreply.github.com>
2024-01-18 23:36:38 +0100

    ✨ feature: added index file for data

---
+++ src/data/index.ts
@@ -0,0 +1,19 @@
+/**
+ *  Project: manoloramos.dev
+ *  Author: Manolo Ramos
+ *  Copyright: (C) 2024 Manolo Ramos - All Rights Reserved
+ * 
+ *  File created on: 18-1-2024 23:26:23 
+ *  Last modified on: 18-1-2024 23:33:22 
+ * 
+ *  Description: Index file for Data elements.
+ */
+
+import footerNavItems from "./navigation/footerItems";
+import { externalNavItems, mainNavItems } from "./navigation/navigationItems";
+
+export { 
+	mainNavItems, 
+	externalNavItems,
+	footerNavItems
+};
