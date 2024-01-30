f0ab2195832e39658170c5275c5606c1d28388e9
Manolo Ramos <35995905+manoloramos@users.noreply.github.com>
2024-01-30 20:21:11 +0100

    🚧 wip: arranging theme values

--- src/styles/theme/Theme.tsx
+++ src/styles/theme/Theme.tsx
@@ -4,67 +4,12 @@
  *  Copyright: (C) 2023 Manolo Ramos - All Rights Reserved
  * 
  *  File created on: 7-12-2023 20:11:13 
- *  Last modified on: 28-1-2024 11:05:23 
+ *  Last modified on: 29-1-2024 11:40:20 
  * 
  *  Description: Defines all the available themes for the App.
  */
 
 import { ThemeCollection } from "@ts/interfaces/theming";
-
-export const theme: ThemeCollection = {
-	light: {
-		colorScheme: 'light',
-		general: {
-			background: ''
-		},
-		card: {
-			background: ''
-		},
-		button: {
-			background: ''
-		},
-		headingText: {
-			color: ''
-		},
-		baseText: {
-			color: ''
-		},
-		link: {
-			color: '',
-			hover: ''
-		},
-		border: {
-			color: '',
-			hover: ''
-		}
-	},
-	dark: {
-		colorScheme: 'dark',
-		general: {
-			background: '#242424'
-		},
-		card: {
-			background: ''
-		},
-		button: {
-			background: ''
-		},
-		headingText: {
-			color: ''
-		},
-		baseText: {
-			color: 'rgba(255, 255, 255, 0.87)'
-		},
-		link: {
-			color: '',
-			hover: ''
-		},
-		border: {
-			color: '',
-			hover: ''
-		}
-	}
-};
 
 export const general = {
 	borders: {
@@ -79,6 +24,9 @@ export const structure = {
 		minHeight: '100vh',
 		display: 'flex',
 		flexFlow: 'column nowrap'
+	},
+	root: {
+		maxWidth: '1200px'
 	}
 };
 
@@ -106,8 +54,17 @@ export const colors = {
 	blue: {
 
 	},
+	white: {
+		'base': '#FFF'
+	},
 	gray: {
-		'0': '#f8f9fa'
+		'brighter': '#F8F9FA',
+		'bright': '#f3f6f9',
+		'border': '#DEE2E6',
+		'borderHover': '#CED4DA',
+		'base': '#495057',
+		'dark': '#212529',
+		'darker': '#242424'
 	},
 	misc: {
 		indigo: ''
@@ -118,4 +75,60 @@ export const fonts = {
 	base: '',
 	heading: '',
 	monospace: '',
+};
+
+export const theme: ThemeCollection = {
+	light: {
+		colorScheme: 'light',
+		general: {
+			background: colors.white.base
+		},
+		card: {
+			background: colors.gray.brighter
+		},
+		button: {
+			color: colors.gray.base,
+			background: colors.gray.bright
+		},
+		headingText: {
+			color: colors.gray.dark
+		},
+		baseText: {
+			color: colors.gray.base
+		},
+		link: {
+			color: '',
+			hover: ''
+		},
+		border: {
+			color: '',
+			hover: ''
+		}
+	},
+	dark: {
+		colorScheme: 'dark',
+		general: {
+			background: colors.gray.darker
+		},
+		card: {
+			background: ''
+		},
+		button: {
+			background: ''
+		},
+		headingText: {
+			color: ''
+		},
+		baseText: {
+			color: 'rgba(255, 255, 255, 0.87)'
+		},
+		link: {
+			color: '',
+			hover: ''
+		},
+		border: {
+			color: '',
+			hover: ''
+		}
+	}
 };
